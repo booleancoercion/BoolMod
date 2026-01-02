47413e1314d739fe3a1720bb5bcaea31e63f111c
booleancoercion <booleancoercion@gmail.com>
2026-01-02 23:55:32 +0200

    Updated the mod to 1.21.1

--- src/main/java/dev/boolco/boolmod/BoolMod.java
+++ src/main/java/dev/boolco/boolmod/BoolMod.java
@@ -4,20 +4,24 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.mojang.brigadier.context.CommandContext;
 
 import eu.pb4.stylednicknames.NicknameHolder;
 
-import static net.minecraft.server.command.CommandManager.*;
+import static net.minecraft.commands.Commands.*;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
 public class BoolMod implements ModInitializer {
     // This logger is used to write text to the console and the log file.
@@ -25,40 +29,47 @@ public class BoolMod implements ModInitializer {
     // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("boolmod");
 
+    private boolean isStyledNicknamesLoaded = false;
+
     @Override
     public void onInitialize() {
-        boolean isStyledNicknamesLoaded = FabricLoader.getInstance().isModLoaded("styled-nicknames");
+        isStyledNicknamesLoaded = FabricLoader.getInstance().isModLoaded("styled-nicknames");
 
-        CommandRegistrationCallback.EVENT
-                .register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("list").then(literal("json")
-                        .executes(context -> {
-                            MinecraftServer server = context.getSource().getServer();
+        CommandRegistrationCallback.EVENT.register(
+                (dispatcher, registryAccess, environment) -> {
+                    dispatcher.register(literal("list").then(literal("json").executes(this::runListJson)));
+                });
+    }
 
-                            JsonObject obj = new JsonObject();
-                            obj.addProperty("current_players", server.getCurrentPlayerCount());
-                            obj.addProperty("max_players", server.getMaxPlayerCount());
+    private int runListJson(CommandContext<CommandSourceStack> context) {
+        MinecraftServer server = context.getSource().getServer();
+        PlayerList playerList = server.getPlayerList();
 
-                            List<ServerPlayerEntity> players = server.getPlayerManager().getPlayerList();
-                            players.sort(Comparator.comparing(player -> player.getName().getString()));
+        JsonObject obj = new JsonObject();
+        obj.addProperty("current_players", playerList.getPlayerCount());
+        obj.addProperty("max_players", playerList.getMaxPlayers());
 
-                            JsonArray arr = new JsonArray();
-                            for (ServerPlayerEntity player : players) {
-                                JsonObject playerObject = new JsonObject();
-                                playerObject.addProperty("name", player.getName().getString());
-                                if (isStyledNicknamesLoaded) {
-                                    playerObject.addProperty("nickname", NicknameHolder.of(player).styledNicknames$get());
-                                    playerObject.add("nickname_styled", Text.Serializer.toJsonTree(NicknameHolder.of(player).styledNicknames$getOutput()));
-                                }
-                                playerObject.addProperty("uuid", player.getUuidAsString());
+        ArrayList<ServerPlayer> players = new ArrayList<ServerPlayer>(playerList.getPlayers());
+        players.sort(Comparator.comparing(player -> player.getName().getString()));
 
-                                arr.add(playerObject);
-                            }
+        JsonArray arr = new JsonArray();
+        for (ServerPlayer player : players) {
+            JsonObject playerObject = new JsonObject();
+            playerObject.addProperty("name", player.getName().getString());
+            if (isStyledNicknamesLoaded) {
+                playerObject.addProperty("nickname", NicknameHolder.of(player).styledNicknames$get());
+            }
+            playerObject.addProperty("uuid", player.getStringUUID());
 
-                            obj.add("list", arr);
+            arr.add(playerObject);
+        }
 
-                            context.getSource().sendMessage(Text.literal(obj.toString()));
+        obj.add("list", arr);
 
-                            return 1;
-                        }))));
+        context.getSource().sendSuccess(() -> {
+            return Component.literal(obj.toString());
+        }, true);
+
+        return 1;
     }
 }
